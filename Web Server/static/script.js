e94e940b1ec5481d8487d2405aab34ecdc6fda63
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-01 16:24:50 +0000

    Format web server

--- Web Server/static/script.js
+++ Web Server/static/script.js
@@ -1,11 +1,11 @@
-jQuery.getJSON("static/gpio_pins.json", function(gpioPinNumbers) {
+jQuery.getJSON("static/gpio_pins.json", function (gpioPinNumbers) {
 	gpioPinNumbers.forEach(pinNum => {
 		const gpioPins = document.getElementById("gpio-pins");
 		const list = document.createElement("li");
 		const checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.dataset.gpiopin = pinNum;
-		checkbox.onclick = function() {
+		checkbox.onclick = function () {
 			makePostRequest(checkbox);
 		};
 		list.innerHTML = "GPIO " + pinNum;
@@ -20,16 +20,16 @@ function makePostRequest(checkbox) {
 		gpioPin: parseInt(checkbox.dataset.gpiopin),
 		writeValue: checkbox.checked ? 1 : 0,
 	};
-	
+
 	$.ajax({
 		type: "POST",
 		contentType: "application/json",
 		url: "/",
-		data : JSON.stringify(data),
-		success: function() {
+		data: JSON.stringify(data),
+		success: function () {
 			console.log("Success at post request");
 		},
-		error: function() {
+		error: function () {
 			console.log("Error at post request");
 		},
 	});
--- Web Server/web_server.py
+++ Web Server/web_server.py
@@ -3,33 +3,36 @@ import json
 import RPi.GPIO as GPIO
 
 with open("config.json", "r") as file:
-	CONFIG = json.load(file)
+    CONFIG = json.load(file)
 server_host = CONFIG["server_host"]
 server_port = CONFIG["server_port"]
 
 with open("static/gpio_pins.json", "r") as file:
-	GPIO_PINS = json.load(file)
+    GPIO_PINS = json.load(file)
 
 app = Flask(__name__, template_folder="")
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-	if request.method == "POST":
-		data = request.json
-		gpio_pin = data["gpioPin"]
-		write_value = data["writeValue"]
-		GPIO.output(gpio_pin, write_value)
-		return ("", 204)
-	else:
-		return render_template("index.html")
+    if request.method == "POST":
+        data = request.json
+        gpio_pin = data["gpioPin"]
+        write_value = data["writeValue"]
+        GPIO.output(gpio_pin, write_value)
+        return ("", 204)
+    else:
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(GPIO_PINS, GPIO.OUT)
-	
-	try:
-		app.run(host=server_host, port=server_port, debug=True, use_reloader=False)
-	finally:
-		GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PINS, GPIO.OUT)
+
+    try:
+        app.run(host=server_host, port=server_port,
+                debug=True, use_reloader=False)
+    finally:
+        GPIO.cleanup()
