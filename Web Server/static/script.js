76085eff017b64ccc340e341a5b4a2a7c63313e0
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-02 21:58:07 +0000

    Refactor web page

--- Web Server/static/script.js
+++ Web Server/static/script.js
@@ -5,17 +5,28 @@ jQuery.getJSON("static/gpio_pins.json", function(gpioPinNumbers) {
 		const pinNum = gpioPinNumbers[i];
 		const currentValue = gpioPinValues[i];
 
-		const list = document.createElement("li");
+		const gpioPin = document.createElement("li");
+		gpioPin.id = "gpio-pin";
+		
+		const label = document.createElement("label");
+		
+		const text = document.createElement("span");
+		text.id = "gpio-text";
+		text.innerHTML = "GPIO " + pinNum
+		label.appendChild(text);
+		
 		const checkbox = document.createElement("input");
+		checkbox.id = "gpio-checkbox";
 		checkbox.type = "checkbox";
 		checkbox.dataset.gpiopin = pinNum;
 		checkbox.checked = currentValue;
 		checkbox.onclick = function() {
 			makePostRequest(checkbox);
-		};
-		list.innerHTML = "GPIO " + pinNum;
-		list.appendChild(checkbox);
-		gpioPins.appendChild(list);
+		}
+		label.appendChild(checkbox);
+		
+		gpioPin.appendChild(label);
+		gpioPins.append(gpioPin);
 	}
 });
 
