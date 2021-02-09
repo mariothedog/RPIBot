087be03bda4fa8e3b49a69e59118bed3425b158a
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-09 23:50:06 +0000

    Add config error catching to the get bits number command

--- Bot/Commands/RPI/get-bits-number.js
+++ Bot/Commands/RPI/get-bits-number.js
@@ -1,9 +1,16 @@
 const util = require("../../util.js");
+const setBitsConfig = require("./set-bits-config");
 
 module.exports = {
 	name: "get-bits-number",
 	description: "Returns the decimal number based on the bits activated",
-	async execute(message) {
+	async execute(message, _args, prefixUsed) {
+		const gpioBitPins = await util.getGPIOBitPins();
+		if (!gpioBitPins) {
+			message.reply(`Please run ${prefixUsed}${setBitsConfig.name} first!`);
+			return true;
+		}
+
 		const number = await util.getBitsNumber();
 		message.reply(`The bits number is ${number}!`);
 
