190e2c9f83649afb6338e9a07cc8511b2a01b4af
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-09 23:48:57 +0000

    Add config error catching to the set bits number command

--- Bot/Commands/RPI/set-bits-number.js
+++ Bot/Commands/RPI/set-bits-number.js
@@ -1,10 +1,17 @@
 const util = require("../../util.js");
+const setBitsConfig = require("./set-bits-config");
 
 module.exports = {
 	name: "set-bits-number",
 	usage: "<Number (n > 0)>",
 	description: "Sets the corresponding bits based on the number in binary",
-	async execute(message, args) {
+	async execute(message, args, prefixUsed) {
+		const gpioBitPins = await util.getGPIOBitPins();
+		if (!gpioBitPins) {
+			message.reply(`Please run ${prefixUsed}${setBitsConfig.name} first!`);
+			return true;
+		}
+
 		let number = parseInt(args[0]);
 		if (isNaN(number)) {
 			return false;
